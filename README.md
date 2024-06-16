f581ce2f82a76a6ca0573deaf53e527fb7421170
jerep <m pip install --upgrade pip>
2024-06-16 18:00:58 +0300

    Committing the Python file and added a readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# picture-manipulation
+# Description
+
+This script iterates over the current working directory, and scans for jpeg files. The jpeg files are changed to A6 size (300 PPI), and a 3 millimetre white border is added to each picture. The new A6 pictures are saved separately in a new directory as JPEG and PDF. The PDF versions are also merged together to a one PDF file called "result.pdf".
+
+## Use case
+
+There was a need to do some simple picture manipulation to hundreds of pictures, and this script was used instead of manually doing the task.
---
+++ picture_resizing.py
@@ -0,0 +1,80 @@
+from PIL import Image
+import img2pdf
+import os
+from pypdf import PdfMerger
+
+def main():
+
+    iterate_directory()
+    convert_img_to_pdf()
+    append_pdf()
+
+
+
+def iterate_directory():
+    for filename in os.listdir():
+        if filename.lower().endswith(('.jpg', '.jpeg')):
+            image = Image.open(filename)
+            new_image = resize_image(image)
+            filename = filename[:-5] #remove .jpeg from the filename
+            save_resized_image(new_image, filename)
+
+def resize_image(img):
+    new_image = img.resize((1240, 1748))
+
+    new_image = add_white_borders(new_image)
+
+    return new_image
+
+def add_white_borders(old_img):
+    new_img = Image.new("RGB", (1310, 1818), "White")
+    box = tuple((n - o) // 2 for n, o in zip((1310, 1818), (1240, 1748)))
+    new_img.paste(old_img, box)
+
+    return new_img
+
+def save_resized_image(img, filename):
+    if not os.path.exists("resized_images"):
+        os.makedirs("resized_images")
+        os.chdir(f'{os.getcwd()}/resized_images')
+        img.save(f'{filename}_a6.jpeg')
+        os.chdir('..')
+    elif os.path.exists("resized_images"):
+        os.chdir(f'{os.getcwd()}/resized_images')
+        img.save(f'{filename}_a6.jpeg')
+        os.chdir('..')
+
+def convert_img_to_pdf():
+    os.chdir(f'{os.getcwd()}/resized_images')
+    for filename in os.listdir():
+        if filename.lower().endswith(('.jpg', '.jpeg')):
+            img = Image.open(filename)
+            filename = filename[:-5]
+            pdf_bytes = img2pdf.convert(img.filename)
+            file = open(f'{filename}.pdf', "wb")
+        
+            file.write(pdf_bytes)
+            
+            img.close()
+            
+            file.close()
+
+def save_pdf_file_names_to_list():
+    lst = []
+    for filename in os.listdir():
+        if filename.lower().endswith(('.pdf')):
+            lst.append(filename)
+    return lst
+
+def append_pdf():
+    merger = PdfMerger()
+    pdfs = save_pdf_file_names_to_list()
+    for pdf in pdfs:
+        merger.append(pdf)
+
+    merger.write("result.pdf")
+    merger.close()
+    print("PDF saved")
+
+main()
+
